4284f18f6b4413fdb787a966d6a32b519205ece8
daberg <daberg@openmailbox.org>
2018-10-06 19:14:09 +0200

    Update benchmark scripts

--- benchmark/completegraphs.py
+++ benchmark/completegraphs.py
@@ -1,6 +1,7 @@
 import goldberg.benchmark
 import graph_tool.generation
 import json
+import sys
 
 
 def benchmark_complete_graph(num_vertices):
@@ -10,20 +11,33 @@ def benchmark_complete_graph(num_vertices):
     capacity = g.new_edge_property("int")
 
     for edge in g.edges():
-        capacity[edge] = 5
+        capacity[edge] = 10
 
     return goldberg.benchmark.benchmark_all(g, g.vertex(0), g.vertex(1), capacity)
 
+
+MAX_VERT = 100
+
+if len(sys.argv) > 2:
+    print("Wrong number of arguments\n\n")
+    print("USAGE")
+    print("\tpython completegraphs.py")
+    print("\tpython completegraphs.py [MAX_VERT]")
+    sys.exit(1)
+
+if len(sys.argv) == 2:
+    MAX_VERT = int(sys.argv[1])
+
 with open('benchmark/results/complete_graph_results.dat', "w") as outfile:
     try :
-        for i in range(2, 150):
+        for i in range(2, MAX_VERT + 1):
             results = benchmark_complete_graph(i)
 
             print("Writing results to  complete_graph_results.dat\n\n\n")
 
-            outfile.write("[{:d}, ".format(i) + json.dumps(results) + "]" + '\n')
+            outfile.write("[{:d}, {:d}, ".format(i, int(i*(i-1)/2)) + json.dumps(results) + "]" + '\n')
+
+        print("Completed benchmarks on complete graphs")
 
     except KeyboardInterrupt:
-        print("Interrupted")
-
-print("Completed benchmarks on complete graphs")
+        print("Interrupted benchmark")
--- benchmark/randomgraphs.py
+++ benchmark/randomgraphs.py
@@ -4,10 +4,10 @@ import graph_tool.topology
 import json
 import pickle
 import random
+import sys
 
 
 def generate_random_graph(num_vertices):
-
     g = graph_tool.generation.random_graph(
         num_vertices,
         lambda : (random.randint(1, num_vertices - 1), random.randint(1, num_vertices - 1)),
@@ -15,19 +15,32 @@ def generate_random_graph(num_vertices):
     )
 
     capacity = g.new_edge_property("int")
-
     for edge in g.edges():
         capacity[edge] = random.randint(1, 50)
 
     return g, capacity
 
 
+MAX_VERT = 100
+NUM_ITER = 25
+
+if len(sys.argv) != 1 and len(sys.argv) != 3:
+    print("Wrong number of arguments\n\n")
+    print("USAGE")
+    print("\tpython randomgraphs.py")
+    print("\tpython randomgraphs.py [MAX_VERT] [NUM_ITER]")
+    sys.exit(1)
+
+if len(sys.argv) == 3:
+    MAX_VERT = int(sys.argv[1])
+    NUM_ITER = int(sys.argv[2])
+
 f_res = open('benchmark/results/random_graph_results.dat', "w")
 f_graphs = open('benchmark/results/random_graphs.dat', "wb")
 
 try:
-    for num_vertices in range(2, 101):
-        for count in range(25):
+    for num_vertices in range(2, MAX_VERT + 1):
+        for count in range(NUM_ITER):
             g, capacity = generate_random_graph(num_vertices)
             print(
                 "Running benchmark on random graph with {:d} vertices and {:d} edges\n".format(
@@ -65,13 +78,13 @@ try:
             print("Serializing graph in random_graph_results.dat\n\n\n")
             pickle.dump(g, f_graphs)
 
+    print("Completed benchmarks on random graphs")
+
 except KeyboardInterrupt:
-    print("Interrupted")
+    print("Benchmark interrupted")
 
 finally:
     f_res.flush()
     f_res.close()
     f_graphs.flush()
     f_graphs.close()
-
-print("Completed benchmarks on random graphs")
